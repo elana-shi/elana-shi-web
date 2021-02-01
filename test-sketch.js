48092a602b41e674fa4a39450e3dbc0eca39db30
elana-shi <77478277+elana-shi@users.noreply.github.com>
2021-01-31 20:29:48 -1000

    Chapter 2 Drawing
    js drawing

---
+++ test-sketch.js
@@ -0,0 +1,29 @@
+function setup(){
+  createCanvas(300,100)
+  background(220,220,220)
+}
+
+function draw(){
+  rect(50,0,200,100)
+  ellipse(300,50,100,100)
+  ellipse(0,50,100,100)
+  
+}
+
+/*
+1. ;
+2. Data types are pre-defined categories that we use to represent information on computers.
+3. The equal sign assigns values to variable names. Variables can be referred to later on in the program. This can be helpful for complex or arbitrary values.
+4. Variable = value
+Variables are usually descriptive nouns, whereas function names are usually verbs describing what it can do.
+5. Comments are descriptions or notes written into the code that do not influence the code function. We can use // or /* to create them. Comments are used to describe what's happening in the code, and are helpful for the author or peer reviewers for later reference.
+6. */
+/*
+function draw(){
+  line(200,35,150,10)
+  line(150,10,100,35)
+  line(100,35,200,60)
+  line(200,60,150,85)
+  line(150,85,100,60)
+}
+*/
